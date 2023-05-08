fe32d02c55ad377ed7439af185e5b2494fa87539
Repository <talush04@gmail.com>
2023-05-08 22:49:06 +0300

    quick push ds

--- quick_push_ds/quick_push_ds_test.cpp
+++ quick_push_ds/quick_push_ds_test.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-#include "quick_pop_ds.hpp"
+#include "quick_push_ds.hpp"
 
 int main()
 {
     DLList<int> dll;
 
-    std::cout << "push should: 5 5 8 8 8 9" << std::endl;
+    std::cout << "push should: 5 3 8 7 7 9" << std::endl;
 
     dll.Push(5);
     std::cout << dll.Peek() << std::endl;
@@ -24,17 +24,13 @@ int main()
     std::cout << '\n' << "size should: 6" << std::endl;
     std::cout << dll.GetSize() << std::endl;
 
-    std::cout << '\n' << "pop should: 8 7 5 3" << std::endl;
+    std::cout << '\n' << "pop should: 9 8 7 7 5" << std::endl;
 
-    dll.Pop();
-    std::cout << dll.Peek() << std::endl;
-    dll.Pop();
-    std::cout << dll.Peek() << std::endl;
-    dll.Pop();
-    std::cout << dll.Peek() << std::endl;
-    dll.Pop();
-    std::cout << dll.Peek() << std::endl;
-    dll.Pop();
+    std::cout << dll.Pop() << std::endl;
+    std::cout << dll.Pop() << std::endl;
+    std::cout << dll.Pop() << std::endl;
+    std::cout << dll.Pop() << std::endl;
+    std::cout << dll.Pop() << std::endl;
     
     std::cout << '\n' << "size should: 1" << std::endl;
     std::cout << dll.GetSize() << std::endl;
@@ -44,7 +40,7 @@ int main()
     std::cout << '\n' << "size should: 0" << std::endl;
     std::cout << dll.GetSize() << std::endl;
 
-    dll.Pop();
+    //dll.Pop();
 
     return 0;
 }
